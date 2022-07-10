f649692eb8dd273fe7a96477a8ad184d3e9b0686
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2022-07-10 16:36:06 -0500

    initial commit of imported Arduino sketch

---
+++ mbot_factory_firmware/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 7/10/2022 4:14:39 PM
+ * Author : unfor
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
